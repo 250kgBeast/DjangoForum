437e38620685d93b63298ce92d096d72db809057
250kgBeast <59151069+250kgBeast@users.noreply.github.com>
2020-10-29 22:00:00 +0200

    added forms

--- WebBoard/boards/views.py
+++ WebBoard/boards/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 
 def home(request):
@@ -26,29 +27,27 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
 
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first()
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        return redirect('board_topics', pk=board.pk)
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
 
     context = {
-        'board': board
+        'board': board,
+        'form': form
     }
 
     return render(request, 'new_topic.html', context)
