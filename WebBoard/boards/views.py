b29a6b1c9cc4a108619aee88c9715d742ed3595d
250kgBeast <59151069+250kgBeast@users.noreply.github.com>
2020-10-29 20:22:44 +0200

    added board_topics view and reusable template

--- WebBoard/boards/views.py
+++ WebBoard/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Board
 
@@ -11,3 +11,13 @@ def home(request):
     }
 
     return render(request, 'home.html', context)
+
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+
+    context = {
+        'board': board
+    }
+
+    return render(request, 'topics.html', context)
