b971f5a98f3e2a54e5daa5f58a02a7cb03d8454b
250kgBeast <59151069+250kgBeast@users.noreply.github.com>
2020-11-02 00:35:43 +0200

    added CBV for accounts app

--- WebBoard/accounts/views.py
+++ WebBoard/accounts/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView
 
-from .forms import SignUpForm
+from .forms import SignUpForm, UserInformationUpdateForm
 
 
 def signup(request):
@@ -19,3 +23,13 @@ def signup(request):
     }
 
     return render(request, 'sign_up.html', context)
+
+
+@method_decorator(login_required, name='dispatch')
+class UserUpdateView(UpdateView):
+    template_name = 'my_account.html'
+    form_class = UserInformationUpdateForm
+    success_url = reverse_lazy('my_account')
+
+    def get_object(self):
+        return self.request.user
